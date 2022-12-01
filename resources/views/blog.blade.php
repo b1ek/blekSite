f224862c421e2e912b2b7b971eae6da98a3e2cdf
b1ek <creeperywime@gmail.com>
2022-12-01 20:45:01 +1000

    update blog posts design

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -11,17 +11,25 @@
     
     @else
 
-        @foreach ($data as $i => $d)
-            <p style='margin:0;padding:0;padding-top:10px' id='{{$d->id}}'>
-                <span style='font-size:11pt;font-weight:600'>
-                {{$d->title}}
-                </span>
-                by {{$d->author}} <span style='padding-left:5px'></span>
-                {{date('d/M/Y H:i.s', $d->time)}} (<span style='font-size:6pt;user-select:all'>{{$d->time}}</span>)
-                (<a href='#{{$d->id}}' style='font-size:7pt'>permalink</a>)
-            </p>
-            <p style='margin:0;padding:2px 0;padding-bottom:6px;max-height:256px;overflow-y:scroll'>{!!$d->body!!}</p>
-        @endforeach
+        <table class='blog_posts'><tbody>
+            @foreach ($data as $i => $d)
+            <tr>
+                <td class='blog_post'>
+                    <p style='margin:0;padding:0;padding-top:10px' id='{{$d->id}}'>
+                        <span style='font-size:11pt;font-weight:600'>
+                        {{$d->title}}
+                        </span>
+                        by {{$d->author}} <span style='padding-left:5px'></span>
+                        {{date('d/M/Y H:i.s', $d->time)}} (<span style='font-size:6pt;user-select:all'>{{$d->time}}</span>)
+                        (<a href='#{{$d->id}}' style='font-size:7pt'>permalink</a>)
+                    </p>
+                    <p style='margin:0;padding:2px 0;padding-bottom:6px;max-height:256px;overflow-y:scroll'>{!!$d->body!!}</p>
+                </td>
+            </tr>
+            {{-- But it works!™ --}}
+            <tr><td><div style='min-height:12px'></div></td></tr>
+            @endforeach
+        </tbody></table>
 
         @endif
     @endif
